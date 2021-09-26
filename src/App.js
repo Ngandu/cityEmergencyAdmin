9a7f9de95841237b0f4359536b1961ddc7b135b0
Patrick Ngandu <patrickngandumudiayi@gmail.com>
2021-09-26 06:34:00 +0200

    Main setup
    set up reactjs & electron

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mt-2">
+      <button className="btn btn-primary">Boom</button>
+    </div>
+  );
+}
+
+export default App;
